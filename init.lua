2a82d45bb868464f74899814f1af1222582de455
Thomas Rudin <thomas@rudin.io>
2018-05-31 16:18:43 +0200

    ore distribution (huge fields...)

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@
 
 -- http://dev.minetest.net/PerlinNoiseMap
 
-local testparam = {
+-- basic planet material noise
+local base_params = {
    offset = 0,
    scale = 1,
    spread = {x=1024, y=512, z=1024},
@@ -11,10 +12,24 @@ local testparam = {
    persist = 0.6
 }
 
+-- ore params
+local ore_params = {
+   offset = 0,
+   scale = 1,
+   spread = {x=1024, y=512, z=1024},
+   seed = 23085729,
+   octaves = 5,
+   persist = 0.6
+}
+
 minetest.register_on_generated(function(minp, maxp, seed)
-	if minp.y < 100 or minp.y > 180 then
+
+	if minp.y < 100 or minp.y > 280 then
 		return
 	end
+
+	-- colid layer
+	local is_solid = minp.y < 200
 
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
@@ -22,9 +37,15 @@ minetest.register_on_generated(function(minp, maxp, seed)
 
 	local side_length = maxp.x - minp.x + 1 -- 80
 	local map_lengths_xyz = {x=side_length, y=side_length, z=side_length}
-	local perlin_map = minetest.get_perlin_map(testparam, map_lengths_xyz):get3dMap_flat(minp)
 
-	local c_stone = minetest.get_content_id("default:stone")
+	local base_perlin_map = minetest.get_perlin_map(base_params, map_lengths_xyz):get3dMap_flat(minp)
+	local ore_perlin_map = minetest.get_perlin_map(ore_params, map_lengths_xyz):get3dMap_flat(minp)
+
+	local c_base = minetest.get_content_id("default:stone")
+	local c_ore1 = minetest.get_content_id("default:mese")
+	local c_ore2 = minetest.get_content_id("default:goldblock")
+	local c_ore3 = minetest.get_content_id("default:diamondblock")
+	local c_air = minetest.get_content_id("air")
 
 	local i = 1
 	for z=minp.z,maxp.z do
@@ -33,17 +54,35 @@ minetest.register_on_generated(function(minp, maxp, seed)
 
 
 		local index = area:index(x,y,z)
-		local n = perlin_map[i]
-
-		if i < 40 then
-			print("i:"..i.." -> n:"..n)
-		end
 
 		-- higher elevation = lower chance
 		local chance = (y-minp.y) / side_length
 
-		if n > chance then
-			data[index] = c_stone
+		if data[index] == c_air then
+			-- unpopulated node
+
+			local base_n = base_perlin_map[i]
+			local ore_n = ore_perlin_map[i]
+
+			if is_solid or base_n > chance then
+
+				local ore_hit = ore_n - chance
+
+				if ore_hit > 0.2 then
+					data[index] = c_ore3
+
+				elseif ore_hit > -0.3 then
+					data[index] = c_ore2
+
+				elseif ore_hit > -0.5 then
+					data[index] = c_ore1
+
+				else
+					-- base material
+					data[index] = c_base
+				end
+
+			end
 		end
 
 		i = i + 1
